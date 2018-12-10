a4698993ae66aa4c83d4f8e35848a05570679056
thomasms <stainer.tom@gmail.com>
2018-12-10 08:14:58 +0000

    Removed type annotations, since it is only supported from python 3.6 onwards

--- pypact/input/inputdata.py
+++ pypact/input/inputdata.py
@@ -1,5 +1,4 @@
 import os
-from typing import List
 
 from pypact.util.decorators import freeze_it
 from pypact.util.exceptions import PypactOutOfRangeException
@@ -17,7 +16,7 @@ class InventoryType(JSONSerializable):
     def __init__(self):
         # a list of tuples, the first entry is the element/nuclide symbol
         # the second is the value
-        self.entries: List[str, float] = []
+        self.entries = []
 
 @freeze_it
 class MassInventory(InventoryType):
@@ -25,9 +24,9 @@ class MassInventory(InventoryType):
         super().__init__()
 
         # the total mass in kg
-        self.totalMass: float = 0.0
+        self.totalMass = 0.0
 
-    def __str__(self) -> str:
+    def __str__(self):
         strrep = "{} {} {}".format('MASS', self.totalMass, len(self.entries))
         for e in self.entries:
             strrep += "\n{} {}".format(e[0], e[1])
@@ -39,7 +38,7 @@ class FuelInventory(InventoryType):
     def __init__(self):
         super().__init__()
 
-    def __str__(self) -> str:
+    def __str__(self):
         strrep = "{} {}".format('FUEL', len(self.entries))
         for i in self.entries:
             strrep += "\n{} {}".format(i[0], i[1])
@@ -55,51 +54,51 @@ class InputData(JSONSerializable):
             
             Initialises all input data
         """
-        self.name: str = name
+        self.name= name
         
-        self._overwrite: bool             = False
-        self._json: bool                  = False
-        self._ignorecollapse: bool        = True
-        self._ignorecondense: bool        = True
-        self._condense: bool              = False
-        self._binaryxs: bool              = False
-        self._useeaf: bool                = False
-        self._approxgamma: bool           = False
-        self._outputhalflife: bool        = False
-        self._outputhazards: bool         = False
-        self._initialinventory: bool      = False
-        self._readgammagroup: bool        = False
-        self._readspontfission: bool      = False
-        self._ignoreuncert: bool          = False
-        self._enablemonitor: bool         = False
-        self._usecumfissyield: bool       = False
-        self._clearancedata: bool         = False
-        self._loglevel: bool              = LOG_SEVERITY_WARNING
+        self._overwrite             = False
+        self._json                  = False
+        self._ignorecollapse        = True
+        self._ignorecondense        = True
+        self._condense              = False
+        self._binaryxs              = False
+        self._useeaf                = False
+        self._approxgamma           = False
+        self._outputhalflife        = False
+        self._outputhazards         = False
+        self._initialinventory      = False
+        self._readgammagroup        = False
+        self._readspontfission      = False
+        self._ignoreuncert          = False
+        self._enablemonitor         = False
+        self._usecumfissyield       = False
+        self._clearancedata         = False
+        self._loglevel              = LOG_SEVERITY_WARNING
         
         # default is 1.0E-12 barns
-        self._xsthreshold: float         = 1.0e-12
-        self._group: int                 = 0
-        self._projectile: int            = PROJECTILE_NEUTRON
+        self._xsthreshold           = 1.0e-12
+        self._group                 = 0
+        self._projectile            = PROJECTILE_NEUTRON
 
         # set the minimum number of atoms deemed significant for the inventory output
-        self._atomsthreshold: float       = 0.0
+        self._atomsthreshold       = 0.0
         
         # the total mass in grams per cubic centimetre (g/cc)
-        self._density: float              = 0.0
+        self._density              = 0.0
 
-        self._inventoryismass: bool       = False
-        self._inventoryisfuel: bool       = False 
+        self._inventoryismass      = False
+        self._inventoryisfuel      = False 
         
         self._inventorymass: InventoryType = MassInventory()
         self._inventoryfuel: InventoryType = FuelInventory()
     
         # irradiation schedule
         # a list of tuples of (time interval in seconds, flux amplitude)
-        self._irradschedule: List(float, float) = []
+        self._irradschedule = []
         
         # cooling schedule
         # a list of time interval in seconds
-        self._coolingschedule: List(float) = []
+        self._coolingschedule = []
     
     def reset(self):
         """
@@ -118,37 +117,37 @@ class InputData(JSONSerializable):
         # to do
         pass
 
-    def overwriteExisting(self, overwrite: bool = True):
+    def overwriteExisting(self, overwrite = True):
         """
             Enables overwriting of output files
             uses keyword CLOBBER
         """
         self._overwrite = overwrite
 
-    def enableJSON(self, enable: bool = True):
+    def enableJSON(self, enable = True):
         """
             Enables JSON output file
             uses keyword JSON
         """
         self._json = enable
     
-    def enableInitialInventoryInOutput(self, output: bool = True):
+    def enableInitialInventoryInOutput(self, output = True):
         """
             Performs the time = 0, inventory step,
             the initial inventory
         """
         self._initialinventory = output
     
-    def enableHalflifeInOutput(self, output: bool = True):
+    def enableHalflifeInOutput(self, output = True):
         """
             Enables half lives to be written to the output file
         """
         self._outputhalflife = output
     
-    def enableHazardsInOutput(self, output: bool = True):
+    def enableHazardsInOutput(self, output = True):
         self._outputhazards = output
     
-    def doCollapse(self, group: int, binary: bool = False):
+    def doCollapse(self, group, binary = False):
         if self._useeaf and binary:
             raise PypactIncompatibleOptionException("Cannot enable binary format reading when using EAF")
 
@@ -156,64 +155,64 @@ class InputData(JSONSerializable):
         self._group = group
         self._binaryxs = binary
     
-    def useEAFLibraries(self, use: bool = True):
+    def useEAFLibraries(self, use = True):
         if self._binaryxs and use:
             raise PypactIncompatibleOptionException("Cannot enable EAF data libraries with binary format")
 
         self._useeaf = use
     
-    def useCumulativeFissionYieldData(self, use: bool = True):
+    def useCumulativeFissionYieldData(self, use = True):
         self._usecumfissyield = use
     
-    def includeClearanceData(self, include: bool = True):
+    def includeClearanceData(self, include = True):
         self._clearancedata = include
     
-    def doCondense(self, condense: bool = True):
+    def doCondense(self, condense = True):
         self._ignorecondense = False
         self._condense = condense
     
-    def approxGammaSpectrum(self, approxgamma: bool = True):
+    def approxGammaSpectrum(self, approxgamma = True):
         self._approxgamma = approxgamma
     
-    def ignoreUncertainties(self, ignore: bool = True):
+    def ignoreUncertainties(self, ignore = True):
         self._ignoreuncert = ignore
     
-    def setXSThreshold(self, threshold: float):
+    def setXSThreshold(self, threshold):
         self._xsthreshold = threshold
     
-    def setProjectile(self, proj: int):
+    def setProjectile(self, proj):
         if proj not in VALID_PROJECTILES:
             raise PypactInvalidOptionException("{} is not a valid projectile option.")
             
         self._projectile = proj
 
-    def readGammaGroup(self, readgg: bool = True):
+    def readGammaGroup(self, readgg = True):
         self._readgammagroup = readgg
     
-    def enableSystemMonitor(self, enable: bool = True):
+    def enableSystemMonitor(self, enable = True):
         self._enablemonitor = enable
 
-    def setAtomsThreshold(self, threshold: float):
+    def setAtomsThreshold(self, threshold):
         self._atomsthreshold = threshold
     
-    def addIrradiation(self, timeInSecs: float, fluxAmp: float):
+    def addIrradiation(self, timeInSecs, fluxAmp):
         self._irradschedule.append((timeInSecs, fluxAmp))
     
     def resetIrradiation(self):
-        self._irradschedule: List[float, float] = []
+        self._irradschedule = []
     
-    def addCooling(self, timeInSecs: float):
+    def addCooling(self, timeInSecs):
         self._coolingschedule.append(timeInSecs)
     
     def resetCooling(self):
-        self._coolingschedule: List[float] = []
+        self._coolingschedule = []
     
-    def setLogLevel(self, severity: int):
+    def setLogLevel(self, severity):
         if severity < LOG_SEVERITY_FATAL or severity > LOG_SEVERITY_TRACE:
             raise PypactOutOfRangeException("Log level {} not valid.".format(severity))
         self._loglevel = severity
 
-    def setDensity(self, densityInGPCC: float):
+    def setDensity(self, densityInGPCC):
         """
             Sets the density of the target in g/cc
             densityInGPCC: density in g/cc, must be positive
@@ -223,7 +222,7 @@ class InputData(JSONSerializable):
 
         self._density = densityInGPCC
 
-    def setMass(self, totalMassInKg: float):
+    def setMass(self, totalMassInKg):
         """
             Sets the material mode to Mass
             Sets the total mass of the target in kg
@@ -241,10 +240,10 @@ class InputData(JSONSerializable):
             Sets the material mode to Fuel
         """
 
-        self._inventoryismass: bool = False
-        self._inventoryisfuel: bool = True
+        self._inventoryismass = False
+        self._inventoryisfuel = True
 
-    def addIsotope(self, isotope: str, numberOfAtoms: float):
+    def addIsotope(self, isotope: str, numberOfAtoms):
         """
             Add an isotope
             isotope: character symbol of element name, e.g. 'Fe' and the mass number of the isotpe e.g '56'
@@ -257,9 +256,9 @@ class InputData(JSONSerializable):
         self._inventoryfuel.entries.append((isotope, numberOfAtoms))
 
     def clearIsotopes(self):
-        self._inventoryfuel.entries: List[str, float] = []
+        self._inventoryfuel.entries = []
 
-    def addElement(self, element: str, percentage: float = 100.0):
+    def addElement(self, element, percentage = 100.0):
         """
             Add an element
             element: character symbol of element name, e.g. 'Fe'
@@ -271,7 +270,7 @@ class InputData(JSONSerializable):
         self._inventorymass.entries.append((element, percentage))
         
     def clearElements(self):
-        self._inventorymass.entries: List[str, float] = []
+        self._inventorymass.entries = []
     
     def _serialize(self, f):
         """

cf718ca6f6bf5f722ecfc5883c2d56c78d2b33ab
thomasms <stainer.tom@gmail.com>
2019-08-12 09:28:46 +0100

    Release candidate 1.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='pypact',
-      version='1.3.1',
+      version='1.3.2',
       description='The package for reading and manipulating the fispact output text file.',
       url='https://github.com/fispact/pypact',
       author='UKAEA',

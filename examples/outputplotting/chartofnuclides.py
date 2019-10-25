89ae9bdd5b2ff5996080e8787545c10c3da26a7f
thomasms <stainer.tom@gmail.com>
2019-10-25 08:56:50 +0100

    Updated chart of nuclides

--- examples/outputplotting/chartofnuclides.py
+++ examples/outputplotting/chartofnuclides.py
@@ -11,10 +11,10 @@ FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                         '..', '..', 'reference', 'AlVC.json')
 
 PROPERTY = 'atoms'
-MIN_VALUE = 1e2
-MAX_VALUE = 1e22
-Z_RANGE = [0, 30]
-A_RANGE = [0, 60]
+MIN_VALUE = 1e4
+MAX_VALUE = 1e24
+Z_RANGE = [0, 30.5]
+A_RANGE = [0, 60.5]
 ELEMENTS_OF_INTEREST = ['V', 'Al', 'C']
 
 # make the plot
@@ -32,15 +32,14 @@ for d in pp.NUCLIDE_DICTIONARY:
 
         # Create a Rectangle patch
         # and add the patch to the Axes
-        rect = patches.Rectangle((i-0.5, d['Z']-0.5), 1, 1, linewidth=2, edgecolor='k',facecolor='none')
+        rect = patches.Rectangle((i-0.5, d['Z']-0.5), 1, 1, linewidth=1, edgecolor='k',facecolor='none')
         ax.add_patch(rect)
 
-# draw notable elements
-for e in ELEMENTS_OF_INTEREST:
-    z = pp.find_z(e)
-    if z >= Z_RANGE[0] and z <= Z_RANGE[1]:
-        ax.axhline(y=z)
-        ax.text(1, z, e, fontsize=14)
+        rx, ry = rect.get_xy()
+        cx = rx + rect.get_width()/2.0
+        cy = ry + rect.get_height()/2.0
+
+        ax.annotate("{}{}".format(d['element'], i), (cx, cy), color='k', weight='bold', fontsize=5, ha='center', va='center')
 
 # define an empty matrix
 data_matrix = np.zeros(shape=(maxZ+1, maxA+1))
@@ -49,13 +48,13 @@ data_matrix[:, :] = 0.0
 time_text = ax.text(A_RANGE[0]+5, Z_RANGE[1], '', fontsize=18)
 im = plt.imshow(data_matrix, cmap='gnuplot', norm=LogNorm(vmin=MIN_VALUE, vmax=MAX_VALUE))
 ax.invert_yaxis()
+ax.axis('off')
 plt.xlabel("A", fontsize=16)
 plt.ylabel("Z", fontsize=16)
 plt.xlim(A_RANGE)
 plt.ylim(Z_RANGE)
 # plt.title(PROPERTY)
-plt.grid()
-fig.colorbar(im)
+fig.colorbar(im, cax = fig.add_axes([0.91, 0.2, 0.03, 0.6]))
 
 # iterate over data in the file
 with pp.JSONReader(FILENAME) as output:
@@ -73,7 +72,7 @@ with pp.JSONReader(FILENAME) as output:
             timestamp.total_activity))
         return [im, time_text]
 
-    anim = animation.FuncAnimation(fig, animate, output, interval=200, blit=False)
-    # anim.save('./chartofnuclidesanimation.gif', writer='imagemagick', fps=3)
+    anim = animation.FuncAnimation(fig, animate, output, interval=20, blit=False)
+    # anim.save('./chartofnuclidesanimation.gif', writer='imagemagick', fps=10)
 
 plt.show()

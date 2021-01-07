549c9f53959c8801588b3a0cbdb1b9e70da4891b
thomasms <stainer.tom@gmail.com>
2021-01-07 22:20:31 +0100

    Added simple example on dominant nuclides at a given time

---
+++ examples/simple/dominants.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+import pypact as pp
+
+filename = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                        '..', '..', 'reference', 'test31.out')
+
+# get dominants at third (0-based) timestep
+TIMESTEP = 2
+
+# sort by this property
+# atoms, heat, activity, grams, ingestion, etc...
+PROPERTY = 'atoms'
+
+# take top 10 (dominants)
+NDOMINANTS = 10
+
+
+def get_name(nuc):
+    return f"{nuc.element}{nuc.isotope}{nuc.state}"
+
+
+with pp.Reader(filename) as output:
+    assert len(output) > TIMESTEP
+
+    sorted_nuclides = sorted(
+        output[TIMESTEP].nuclides,
+        key=lambda x: getattr(x, PROPERTY),
+        # descending for top ranked - use False for bottom ranked
+        reverse=True
+    )
+
+    print(f"==== TOP {NDOMINANTS} NUCLIDES BY {PROPERTY} ====")
+    for n in sorted_nuclides[:NDOMINANTS]:
+        print(get_name(n), getattr(n, PROPERTY))
